b6ef21e0a70533f2d80996d574c4ca4796634bed
Fatima Abrar <abrarfatima89@gamil.com>
2022-05-21 21:13:34 -0400

    fonts partial set up and initial browser routes set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './styles/App.scss';
+import { BrowserRouter, Route, Switch, Redirect} from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
+    <>
     <div className="App">
       <h1>hello</h1>
     </div>
+
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Login} />
+        <Route path="/register" component={Register} />
+        <Route path='/dashboard' component={Dashboard} />
+        <Redirect to="/" />
+      </Switch>
+    </BrowserRouter>
+    </>
+    
   );
 }
 
